3eef1349735798ce0068ab669a6fd1195b256347
Tobias Wukovitsch <tobias.wukovitsch@univie.ac.at>
2024-05-08 16:36:57 +0200

    basic spring boot setup

---
+++ backend/src/test/java/at/ac/univie/imse/backend/BackendApplicationTests.java
@@ -0,0 +1,9 @@
+package at.ac.univie.imse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+}
