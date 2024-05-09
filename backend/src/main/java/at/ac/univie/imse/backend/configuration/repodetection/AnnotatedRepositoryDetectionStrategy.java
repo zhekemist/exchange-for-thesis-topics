f8d646b89d06afab4ad3653eaa332796cac6b423
Tobias Wukovitsch <tobias.wukovitsch@univie.ac.at>
2024-05-09 13:28:50 +0200

    configured Spring Data REST

---
+++ backend/src/main/java/at/ac/univie/imse/backend/configuration/repodetection/AnnotatedRepositoryDetectionStrategy.java
@@ -0,0 +1,29 @@
+package at.ac.univie.imse.backend.configuration.repodetection;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.data.repository.core.RepositoryMetadata;
+import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
+
+@Slf4j
+@RequiredArgsConstructor
+public class AnnotatedRepositoryDetectionStrategy implements RepositoryDetectionStrategy {
+
+    private final Environment springEnv;
+
+    @Override
+    public boolean isExported(RepositoryMetadata metadata) {
+        Class<?> repositoryInterface = metadata.getRepositoryInterface();
+        ExposedIf exposedIfAnnotation = repositoryInterface.getAnnotation(ExposedIf.class);
+
+        boolean expose = (exposedIfAnnotation != null) &&
+                this.springEnv.getProperty(exposedIfAnnotation.value(), boolean.class, false);
+
+        if (expose){
+            log.info("{} will be exposed.", repositoryInterface.getTypeName());
+        }
+
+        return expose;
+    }
+}
