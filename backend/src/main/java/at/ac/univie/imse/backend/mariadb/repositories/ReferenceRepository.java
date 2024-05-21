a3fe404b2377088b1158008ea1e81e8b1b2815d2
Tobias Wukovitsch <tobias.wukovitsch@univie.ac.at>
2024-05-21 13:43:07 +0200

    configured Spring Data REST to embed literature references

--- backend/src/main/java/at/ac/univie/imse/backend/mariadb/repositories/ReferenceRepository.java
+++ backend/src/main/java/at/ac/univie/imse/backend/mariadb/repositories/ReferenceRepository.java
@@ -1,9 +1,7 @@
 package at.ac.univie.imse.backend.mariadb.repositories;
 
-import at.ac.univie.imse.backend.configuration.repodetection.ExposeViaRestIf;
 import at.ac.univie.imse.backend.mariadb.datamodel.LiteratureReference;
 import org.springframework.data.repository.CrudRepository;
 
-@ExposeViaRestIf("dbs.maria.rest.expose")
 public interface ReferenceRepository extends CrudRepository<LiteratureReference, Long> {
 }
---
+++ backend/src/main/java/at/ac/univie/imse/backend/mariadb/rest/ThesisTopicEventHandler.java
@@ -0,0 +1,29 @@
+package at.ac.univie.imse.backend.mariadb.rest;
+
+import at.ac.univie.imse.backend.mariadb.datamodel.LiteratureReference;
+import at.ac.univie.imse.backend.mariadb.datamodel.ThesisTopic;
+import at.ac.univie.imse.backend.mariadb.repositories.ReferenceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.rest.core.annotation.HandleAfterCreate;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+
+@Profile("expose-maria")
+@Component
+@RepositoryEventHandler
+public class ThesisTopicEventHandler {
+
+    @Autowired
+    ReferenceRepository referenceRepository;
+
+    @HandleAfterCreate
+    public void handleThesisTopicSave(ThesisTopic topic) {
+        for (var referenceEntry : topic.getReferences().entrySet()) {
+            LiteratureReference reference = referenceEntry.getValue();
+            reference.setReferenceNumber(referenceEntry.getKey());
+            reference.setTopic(topic);
+            referenceRepository.save(reference);
+        }
+    }
+}
