fa785f4dd68c2c65c1fad39f40ee04a77cea97de
Tobias Wukovitsch <tobias.wukovitsch@univie.ac.at>
2024-05-21 14:09:08 +0200

    change category repository to the pageable one

--- backend/src/main/java/at/ac/univie/imse/backend/mariadb/repositories/CategoryRepository.java
+++ backend/src/main/java/at/ac/univie/imse/backend/mariadb/repositories/CategoryRepository.java
@@ -2,8 +2,8 @@ package at.ac.univie.imse.backend.mariadb.repositories;
 
 import at.ac.univie.imse.backend.configuration.repodetection.ExposeViaRestIf;
 import at.ac.univie.imse.backend.mariadb.datamodel.Category;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 @ExposeViaRestIf("dbs.maria.rest.expose")
-public interface CategoryRepository extends CrudRepository<Category, Long> {
+public interface CategoryRepository extends PagingAndSortingRepository<Category, Long> {
 }
