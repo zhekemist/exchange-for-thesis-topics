0faf990a74ebd13ae0aa80314693b474b3235d6e
Tobias Wukovitsch <tobias.wukovitsch@univie.ac.at>
2024-05-24 23:30:51 +0200

    Added getters and setters

--- backend/src/main/java/at/ac/univie/imse/backend/mariadb/datamodel/LiteratureReferenceId.java
+++ backend/src/main/java/at/ac/univie/imse/backend/mariadb/datamodel/LiteratureReferenceId.java
@@ -2,11 +2,15 @@ package at.ac.univie.imse.backend.mariadb.datamodel;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
 @EqualsAndHashCode
 @AllArgsConstructor
+@Getter
+@Setter
 public class LiteratureReferenceId implements Serializable {
     private long referenceNumber;
     private long topic;
